b51be187b80c27a1e7c60bd2447e9602c140aa04
shishupaltech <veerutech75@gmail.com>
2022-11-08 15:51:27 +0530

    theme added

--- lib/main.dart
+++ lib/main.dart
@@ -5,44 +5,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_practice/pages/home_page.dart';
 import 'package:flutter_practice/pages/login_page.dart';
 import 'package:flutter_practice/utils/routes.dart';
+import 'package:flutter_practice/widgets/themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main()
-{
+void main() {
   runApp(myApp());
 }
 
 class myApp extends StatelessWidget {
-  
-  
   @override
   Widget build(BuildContext context) {
-    
-
     return MaterialApp(
-      
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
-        fontFamily: GoogleFonts.lato().fontFamily,
-        
-        ),
-        debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData( 
-        brightness: Brightness.dark
-      ),
+      theme: MyTheme.lightTheme(context),
+      debugShowCheckedModeBanner: false,
+      darkTheme: MyTheme.darkTheme(context),
       initialRoute: MyRoutes.homeRoute,
-      routes:{
-        "/":(context)=>LoginPage(),
-        "/home":(context)=>HomePage(),
-        "/login":(context)=>LoginPage(),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
         // MyRoutes.homeRoute: (context)=>HomePage(),
         // MyRoutes.loginRoute:(context)=>LoginPage(),
-      } ,
-      
-
+      },
     );
   }
-
-  
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,7 +8,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return Scaffold(
+
       appBar: AppBar(
+        
         title: Text("Catalog App"),
       ),
       body: Center(
