8cff0f364ee0e8977d0d8bc3a26588f220180b6c
shishupaltech <veerutech75@gmail.com>
2022-11-08 21:59:45 +0530

    json property fix

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,32 +16,47 @@ class _HomePageState extends State<HomePage> {
 
   final String name = "shishupal";
 
-  @override 
-  void initState(){
+  @override
+  void initState() {
     super.initState();
     loadData();
   }
-  loadData() async{
-  var catalogJosn= await rootBundle.loadString("assets/files/catelog.json");
-  var decodeData = jsonDecode(catalogJosn);
-  print(decodeData);
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 3));
+    var catalogJosn = await rootBundle.loadString("assets/files/catalog.json");
+
+    final decodeData = jsonDecode(catalogJosn);
+    var productsData = decodeData["products"];
+    print(productsData);
+
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+
+    // print(productsData);
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
+    // final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: ListView.builder(
-        itemCount: dummyList.length,
-        itemBuilder: (context, index) {
-          return ItemWidget(
-            item: dummyList[index],
-          );
-        },
-      ),
+      body: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+          ? ListView.builder(
+              itemCount: CatalogModel.items!.length,
+              itemBuilder: (context, index) {
+                return ItemWidget(
+                  item: CatalogModel.items![index],
+                );
+              },
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
       drawer: MyDrawer(),
     );
   }
