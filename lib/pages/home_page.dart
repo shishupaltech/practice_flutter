befeba02c479be0c09b8d7376267721383a332c7
shishupaltech <veerutech75@gmail.com>
2022-11-08 17:40:53 +0530

    card is ready now

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,29 @@
 import "package:flutter/material.dart";
 import 'package:flutter_practice/widgets/drawer.dart';
+
+import '../models/catalog.dart';
+import '../widgets/item_widget.dart';
 class HomePage extends StatelessWidget {
   final num day = 30;
   final String name = "shishupal";
 
   @override
   Widget build(BuildContext context) {
-    
+    final dummyList = List.generate(50,(index)=>CatalogModel.items[0]);
     return Scaffold(
 
       appBar: AppBar(
         
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child:Container(
-          child: Text("Welcome to $day days of flutter by $name"),
-          )
-      ),
+      body: ListView.builder(
+
+        itemCount:dummyList.length,
+        itemBuilder: (context,index){
+          return ItemWidget( 
+            item:dummyList[index],
+          );
+      },),
       drawer: MyDrawer(),
     );
   }
